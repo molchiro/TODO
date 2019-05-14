f1605de6bf0d079ab7f4ca8c1a30556b060bc36b
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-05-15 00:17:06 +0900

    symplify with pug

---
+++ components/MyComponent.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div
+    div Prop {{inherited}}
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class MyComponent extends Vue {
+  @Prop()
+  public inherited!: string
+}
+</script>
